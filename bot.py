5a6a710c968974f171dc5ec4597a1db7b0deffe2
abdalsalamfalah <37448148+abdalsalamfalah@users.noreply.github.com>
2021-01-07 00:45:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,14 +9,16 @@ global v
 
 def new_member(bot, update):
     user = update.message.from_user
-    update.message.reply_text('اهلا وسهلا نورت/ي قروبنا حياك الله {} {} '.format(user['first_name'], user['last_name']))
+
+    update.message.reply_text('اهلا اهلا {} {} '.format(user['first_name'], user['last_name']))
 
 
 def start(bot, update):
     user = update.message.from_user
     if update.message.text == 'مرحبا':
-        v='اهلا وسهلا جميلتنا {} اذكري رمز الدخول ال لي موجود في الرسالة التي دخلتِي من خلالها'.format(user['first_name'])
+        v='اهلا وسهلا جميلتنا نورتينا {} اذكري رمز الدخول الي موجود في الرسالة الي دخلتي من خلالها'.format(user['first_name'])
        # v = 'اهلا اهلا {} {} '.format(user['first_name'], user['last_name'])
+        speak(v,bot, update)
 
     elif update.message.text == 'اخرس':
         v = 'اهـــا أوكيه'
@@ -26,7 +28,8 @@ def start(bot, update):
         v = 'اصبر وين بأجيك'
     elif update.message.text == 'بوت':
         v = ' مب بوت ياحمار'
-    bot.send_message(chat_id=update.message.chat_id,text=v, )
+    bot.send_message(chat_id=update.message.chat_id,
+                     text=v, )
 
 
 updater.dispatcher.add_handler(MessageHandler(Filters.status_update.new_chat_members, new_member))
@@ -34,4 +37,3 @@ start_handler = MessageHandler(Filters.text, start)
 dispatcher.add_handler(start_handler)
 updater.start_polling()
 updater.idle()
-
